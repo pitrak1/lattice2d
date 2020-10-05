8e6752fddfdb97a0f4aa9bcd1cf1ef978f15256d
Nick Pitrak <pitrak1@gmail.com>
2020-10-04 19:51:17 -0500

    clean up nodes test

--- unit/nodes_test.py
+++ unit/nodes_test.py
@@ -1,7 +1,46 @@
 import types
+import pytest
 
 from lattice2d.command import Command
 from lattice2d.nodes import Node, RootNode
+
+@pytest.fixture
+def create_mock_child():
+	def _create_mock_child(mocker):
+		child = types.SimpleNamespace()
+		child.on_command = mocker.stub()
+		child.on_update = mocker.stub()
+		child.on_draw = mocker.stub()
+		return child
+	return _create_mock_child
+
+@pytest.fixture
+def create_node_with_mock_children(create_mock_child):
+	def _create_node_with_mock_children(mocker, number_of_children):
+		node = MyNode()
+		children = []
+
+		for i in range(0, number_of_children):
+			child = create_mock_child(mocker)
+			node.add_child(f'child{i}', child)
+			children.append(child)
+
+		return node, children
+	return _create_node_with_mock_children
+
+@pytest.fixture
+def create_root_node_with_mock_children(create_mock_child):
+	def _create_node_with_mock_children(mocker, number_of_children):
+		node = MyRootNode()
+		children = []
+
+		for i in range(0, number_of_children):
+			child = create_mock_child(mocker)
+			node.add_child(f'child{i}', child)
+			children.append(child)
+
+		return node, children
+	return _create_node_with_mock_children
 
 
 class MyNode(Node):
@@ -24,6 +63,9 @@ class MyRootNode(RootNode):
 	def add_child(self, key, child):
 		self._children[key] = child
 
+	def add_command_by_name(self, name):
+		self.add_command(Command(name, {}))
+
 
 class TestNode:
 	class TestOnCommand:
@@ -34,96 +76,52 @@ class TestNode:
 			assert node.called
 
 	class TestDefaultHandler:
-		def test_calls_on_command_for_all_children(self, mocker):
-			child = types.SimpleNamespace()
-			child.on_command = mocker.stub()
-
-			node = MyNode()
-			node.add_child('key', child)
-
+		def test_calls_on_command_for_all_children(self, mocker, create_node_with_mock_children):
+			node, children = create_node_with_mock_children(mocker, 1)
 			command = Command('test_command_2', {})
 			node.on_command(command)
-			child.on_command.assert_called_once_with(command)
+			children[0].on_command.assert_called_once_with(command)
 
-		def test_returns_false_if_all_children_return_false_by_default(self):
-			child_1 = types.SimpleNamespace()
-			child_1.on_command = lambda comm: False
-
-			child_2 = types.SimpleNamespace()
-			child_2.on_command = lambda comm: False
-
-			node = MyNode()
-			node.add_child('key', child_1)
-			node.add_child('key2', child_2)
-
+		def test_returns_false_if_all_children_return_false_by_default(self, mocker, create_node_with_mock_children):
+			node, children = create_node_with_mock_children(mocker, 2)
+			for child in children:
+				child.on_command = lambda comm: False
 			command = Command('test_command', {})
 			assert not node.default_handler(command)
 
-		def test_returns_true_if_any_children_return_true_by_default(self):
-			child_1 = types.SimpleNamespace()
-			child_1.on_command = lambda comm: False
-
-			child_2 = types.SimpleNamespace()
-			child_2.on_command = lambda comm: True
-
-			node = MyNode()
-			node.add_child('key', child_1)
-			node.add_child('key2', child_2)
-
+		def test_returns_true_if_any_children_return_true_by_default(self, mocker, create_node_with_mock_children):
+			node, children = create_node_with_mock_children(mocker, 2)
+			children[0].on_command = lambda comm: False
+			children[1].on_command = lambda comm: True
 			command = Command('test_command', {})
 			assert node.default_handler(command)
 
 	class TestOnUpdate:
-		def test_calls_on_update_for_all_children(self, mocker):
-			child = types.SimpleNamespace()
-			child.on_update = mocker.stub()
-
-			node = MyNode()
-			node.add_child('key', child)
-
+		def test_calls_on_update_for_all_children(self, mocker, create_node_with_mock_children):
+			node, children = create_node_with_mock_children(mocker, 2)
 			node.on_update(1234)
-			child.on_update.assert_called_once_with(1234)
+			for child in children:
+				child.on_update.assert_called_once_with(1234)
 
 	class TestOnDraw:
-		def test_calls_on_draw_for_all_children(self, mocker):
-			child = types.SimpleNamespace()
-			child.on_draw = mocker.stub()
-
-			node = MyNode()
-			node.add_child('key', child)
-
+		def test_calls_on_draw_for_all_children(self, mocker, create_node_with_mock_children):
+			node, children = create_node_with_mock_children(mocker, 2)
 			node.on_draw()
-			child.on_draw.assert_called_once()
+			for child in children:
+				child.on_draw.assert_called_once()
 
 
 class TestRootNode:
 	def test_calls_on_command_on_self_for_every_command_on_update(self, mocker):
 		root_node = MyRootNode()
 		mocker.patch.object(root_node, 'on_command')
-
-		command_1 = Command('some_command_type', {})
-		root_node.add_command(command_1)
-
-		command_2 = Command('some_other_command_type', {})
-		root_node.add_command(command_2)
-
+		root_node.add_command_by_name('some_command_type')
+		root_node.add_command_by_name('some_other_command_type')
 		root_node.on_update()
 		assert root_node.on_command.call_count == 2
 
-	def test_calls_on_update_for_children_on_update(self, mocker):
-		root_node = MyRootNode()
-
-		child_1 = types.SimpleNamespace()
-		child_1.on_command = mocker.stub()
-		child_1.on_update = mocker.stub()
-
-		child_2 = types.SimpleNamespace()
-		child_2.on_command = mocker.stub()
-		child_2.on_update = mocker.stub()
-
-		root_node.add_child('key', child_1)
-		root_node.add_child('key2', child_2)
-
+	def test_calls_on_update_for_children_on_update(self, mocker, create_root_node_with_mock_children):
+		root_node, children = create_root_node_with_mock_children(mocker, 2)
 		root_node.on_update(1234)
-		child_1.on_update.assert_called_once_with(1234)
-		child_2.on_update.assert_called_once_with(1234)
+		children[0].on_update.assert_called_once_with(1234)
+		children[1].on_update.assert_called_once_with(1234)
